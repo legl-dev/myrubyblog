cbfa26f2dd62db6354aa7b5fa042ec5486c3d342
Luis Garces <luisgarces@Luiss-MacBook-Pro.local>
2021-04-23 12:03:02 -0300

    db models and migrations

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = "This some sample text for our awesome Ruby Blog";
+        @content_second = "This another sample text for our awesome Ruby Blog";
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+end
